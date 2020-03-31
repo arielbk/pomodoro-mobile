858e0315b1ee066b9dce6221d48857f0a92e407c
arielbk <buchwald.kearney@gmail.com>
2020-03-31 22:11:18 +0200

    Separate timer logic into a hook

---
+++ components/utilities/usePomodoroTimer.js
@@ -0,0 +1,105 @@
+import { useReducer, useEffect, useRef } from 'react';
+
+export const initialState = {
+  // focus time length
+  focusSpan: 1500,
+  // break time length
+  breakSpan: 300,
+  // current mode (focus or break)
+  currentMode: 'focus',
+  // whether the timer is running
+  isRunning: false,
+  // time when timer last began
+  startTime: null,
+  // remaining time displayed
+  remainingTime: 1500,
+  // flag becomes true when timer mode finishes
+  isFinished: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'start':
+      return {
+        ...state,
+        isRunning: true,
+        startTime: action.payload,
+        modeCompleted: null,
+        isFinished: false,
+      }
+    case 'pause':
+      return {
+        ...state,
+        isRunning: false,
+        startTime: null,
+      }
+    case 'set_remaining':
+      if (action.payload > 0) return {
+        ...state,
+        remainingTime: action.payload,
+    }
+    //! if remaining time hits 0 finish fires
+    case 'finish':
+      return {
+        ...state,
+        isRunning: false,
+        startTime: null,
+        remainingTime: state.currentMode === 'focus'
+          ? state.breakSpan
+          : state.focusSpan,
+        currentMode: state.currentMode === 'focus'
+          ? 'break'
+          : 'focus',
+        isFinished: true,
+      }
+    case 'reset':
+      return {...initialState}
+    default:
+      throw new Error();
+  }
+}
+
+// todo: this should take in a settings object
+const usePomodoroTimer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const interval = useRef();
+
+  useEffect(() => {
+    if (state.isRunning) {
+      interval.current = setInterval(() => {
+        const timeElapsed = new Date().getTime() - state.startTime;
+        dispatch({
+          type: 'set_remaining',
+          payload: state.remainingTime - timeElapsed,
+        });
+      }, 30)
+    } else {
+      clearInterval(interval.current);
+    }
+  }, [state.isRunning]);
+
+  const totalTime = state.currentMode === 'focus'
+    ? state.focusSpan
+    : state.breakSpan;
+
+  const playPause = () => {
+    const now = new Date().getTime();
+    dispatch({
+      type: state.isRunning ? 'pause' : 'start',
+      payload: now,
+    });
+  };
+
+  const reset = () => {
+    dispatch({ type: 'reset'});
+  };
+
+  return {
+    ...state,
+    totalTime,
+    playPause,
+    reset,
+  }
+}
+
+export default usePomodoroTimer;
--- components/views/MainView.js
+++ components/views/MainView.js
@@ -1,40 +1,25 @@
-import React, { useReducer, useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, StatusBar, Dimensions, Platform } from 'react-native';
 import { Audio } from 'expo-av';
 import { Ionicons } from '@expo/vector-icons';
 
-import reducer, { initialState } from '../reducers/countdown';
 import Timer from '../shared/Timer';
 import Progress from '../shared/Progress';
 import usePomodoroCount from '../utilities/usePomodoroCount';
+import usePomodoroTimer from '../utilities/usePomodoroTimer';
 
 const screen = Dimensions.get('window');
 const endSound = new Audio.Sound();
 endSound.loadAsync(require('../../assets/sounds/levelup.mp3'));
 
 export default function MainView() {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const interval = useRef();
   const [pomodoroCount, incrementPomodoro] = usePomodoroCount();
-
-  useEffect(() => {
-    if (state.isRunning) {
-      interval.current = setInterval(() => {
-        const timeElapsed = new Date().getTime() - state.startTime;
-        dispatch({
-          type: 'set_remaining',
-          payload: state.remainingTime - timeElapsed,
-        });
-      }, 30)
-    } else {
-      clearInterval(interval.current);
-    }
-  }, [state.isRunning])
+  const timer = usePomodoroTimer();
   
   useEffect(() => {
-    if (state.isFinished) {
+    if (timer.isFinished) {
       // if focus period has just finished
-      if (state.currentMode === 'break') incrementPomodoro();
+      if (timer.currentMode === 'break') incrementPomodoro();
       // play finishing sound
       try {
         endSound.replayAsync();
@@ -42,11 +27,7 @@ export default function MainView() {
         console.error(error);
       }
     }
-  }, [state.isFinished]);
-
-  const totalTime = state.currentMode === 'focus'
-    ? state.focusSpan
-    : state.breakSpan;
+  }, [timer.isFinished]);
 
   return (
     <>
@@ -63,16 +44,14 @@ export default function MainView() {
             </TouchableOpacity>
           </View>
           <Text style={styles.projectText}>Project name</Text>
-          <Timer millis={state.remainingTime} />
-          <Progress totalTime={totalTime} remainingTime={state.remainingTime} isRunning={state.isRunning} />
-          <Text style={styles.currentMode}>{state.currentMode}</Text>
+          <Timer millis={timer.remainingTime} />
+          <Progress totalTime={timer.totalTime} remainingTime={timer.remainingTime} isRunning={timer.isRunning} />
+          <Text style={styles.currentMode}>{timer.currentMode}</Text>
         </View>
 
         <View style={styles.footerContent}>
           
-          <TouchableOpacity
-            onPress={() => dispatch({type: 'reset'})}
-          >
+          <TouchableOpacity onPress={timer.reset}>
             <View style={styles.button}>
               <Text>
                 {
@@ -88,19 +67,11 @@ export default function MainView() {
             {pomodoroCount} / 8
           </Text>
 
-          <TouchableOpacity
-            onPress={() => {
-              const now = new Date().getTime();
-              dispatch({
-                type: state.isRunning ? 'pause' : 'start',
-                payload: now,
-              });
-            }
-          }>
+          <TouchableOpacity onPress={timer.playPause}>
             <View style={styles.button}>
               <Text>
                 {
-                  state.isRunning
+                  timer.isRunning
                     ? Platform.OS === 'android'
                       ? <Ionicons name="md-pause" color="#676767" size={36} />
                       : <Ionicons name="ios-pause" color="#676767" size={36} />
